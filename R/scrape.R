52855f5b0981fb11df3173ff03025bbed7229a9f
Sam <sam.cooke@education.gov.uk>
2024-03-06 17:18:49 +0000

    map cleaning part 1

--- R/scrape.R
+++ R/scrape.R
@@ -3,7 +3,8 @@ library(tidyverse)
 library(gsheet)
 
 
-url <- "https://docs.google.com/spreadsheets/d/1E-z17KdP1RNzUOwubxs9q79fF119pSg5/edit#gid=2061677816"
+qs_url <- "https://docs.google.com/spreadsheets/d/1E-z17KdP1RNzUOwubxs9q79fF119pSg5/edit#gid=2061677816"
+maps_url <- "https://docs.google.com/spreadsheets/d/1E-z17KdP1RNzUOwubxs9q79fF119pSg5/edit#gid=1514390075"
 
 hood_reader <- function(url, sheet){
   data <- gsheet::construct_download_url(url = url, format = "csv")
@@ -56,3 +57,54 @@ question_cleaner <- function(input){
   
   data
 }
+
+questions <- hood_reader(url, 1)
+
+data <- question_cleaner(questions)
+
+maps <- hood_reader(maps_url, 1)
+
+
+view(maps[[1]])
+
+
+map_cleaner <- function(input){
+  
+  data <- input[[1]]
+  
+  data |> rename(
+    'id' = 1,
+    'sub'= 2,
+    'sub_time'= 3,
+    'index' = 4,
+    'wkt' = 7,
+    'geojson' = 8,
+    'name' = 9,
+    'desc' = 10,
+    'live' = 11,
+    'work' = 12,
+    'visit' = 13,
+    'leisure' = 14,
+    'other' = 15,
+    'hoods_free' = 16
+  ) |> 
+    select(-desc) |>
+    mutate(name = tolower(name),
+           name = trimws(name, which = 'both'),
+           name = str_remove(name,"\\p{P}")) |> 
+    select(name)
+    
+}
+
+
+map_cleaner(maps) |> group_by(name) |> 
+  summarise(n = n()) |> 
+  arrange(desc(n)) |> view()
+
+
+
+#original 324
+#v1 300
+
+
+nrow(maps[[1]])
